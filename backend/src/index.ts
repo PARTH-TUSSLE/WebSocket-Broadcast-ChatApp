43a427ecbce40ca4475f3861eb279a8d6e3f9e36
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2025-09-10 22:30:02 +0530

    Frontend init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,20 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 3000 });
 
 let userCount = 0;
+let allSockets = [];
 
 wss.on("connection", ( socket ) => {
   userCount ++ ;
+  allSockets.push(socket);
   console.log(`User ${userCount} connected !`);
 
-  
+  socket.on("message", (message) => {
+    console.log(`Message recieved: ${message.toString()}`);
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      setTimeout(() => {
+        s!.send(`${message.toString()} from server`);
+      }, 1000);
+    }
+  })
 })
