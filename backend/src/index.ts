b2e112d5672311298df994f44707880e53ffc38f
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2025-09-11 00:03:41 +0530

    Implemented Rooms for multilple ws

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,40 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 3000 });
 
+interface User  {
+  socket: WebSocket,
+  room: string
+}
+
 let userCount = 0;
-let allSockets = [];
+let allSockets: User[] = [];  // MAPS and RECORDS
 
 wss.on("connection", ( socket ) => {
   userCount ++ ;
-  allSockets.push(socket);
   console.log(`User ${userCount} connected !`);
 
   socket.on("message", (message) => {
-    console.log(`Message recieved: ${message.toString()}`);
-    for (let i = 0; i < allSockets.length; i++) {
-      const s = allSockets[i];
-      setTimeout(() => {
-        s!.send(`${message.toString()} from server`);
-      }, 1000);
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type === "join" ) {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId
+      })
+    }  
+    if (parsedMessage.type === "chat") {
+      const foundUser = allSockets.find((x) => x.socket === socket);
+      const currUserRoom = foundUser ? foundUser.room : undefined;
+
+      for (let i = 0; i < allSockets.length; i++) {
+          //@ts-ignore
+          if (currUserRoom === allSockets[i].room && allSockets[i].socket !== socket) {
+            //@ts-ignore
+            allSockets[i].socket.send(parsedMessage.payload.message);
+          }
+      }
+
     }
-  })
+  });
+  
 })
